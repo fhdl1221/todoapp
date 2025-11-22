90a5c27249cade2e544fe372b756b99981509a3f
fhdl1221 <fhdl353212@gmail.com>
2025-11-22 17:53:07 +0900

    read 1 로직 작성

--- src/main/java/com/example/todoapp/controller/TodoController.java
+++ src/main/java/com/example/todoapp/controller/TodoController.java
@@ -2,17 +2,27 @@ package com.example.todoapp.controller;
 
 import com.example.todoapp.dto.TodoDto;
 import com.example.todoapp.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TodoController {
 
-    @GetMapping("/todos")
-    public String todos() {
+    private final TodoRepository todoRepository = new TodoRepository();
 
+    @GetMapping("/todos")
+    public String todos(Model model) {
+        // 이전에 만들었던 repository와 다른 객체를 사용하면 안됨
+        // TodoRepository todoRepository = new TodoRepository();
+        List<TodoDto> todos = todoRepository.findAll();
+        model.addAttribute("todos", todos);
         return "todos";
     }
 
@@ -28,11 +38,18 @@ public class TodoController {
             Model model
     ) {
         TodoDto todoDto = new TodoDto(null, title, content, false);
-        TodoRepository todoRepository = new TodoRepository();
+        // TodoRepository todoRepository = new TodoRepository();
 
         TodoDto todo = todoRepository.save(todoDto);
         model.addAttribute("todo", todo);
 
         return "create";
+    }
+
+    @GetMapping("/todos/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        TodoDto todo = todoRepository.findById(id);
+        model.addAttribute("todo", todo);
+        return "detail";
     }
 }
